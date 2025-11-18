a7eaad3966e00d64db7f45e3577c8ce3733d93e0
dratsuni <matherthentic@gmail.com>
2025-11-18 11:29:17 +0000

    update

---
+++ Primeiro_Simestre/Logica-de-programacao/exercicio-algoritimos-e-expressoes-aritimeticas/sum.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+
+void clearBuffer();
+int sum(int num1, int num2);
+
+int main(){
+    int num1, num2;
+    
+    printf("Digite o primeiro número: ");
+    scanf("%d", &num1);
+    printf("Digite o segundo numero: ");
+    scanf("%d", &num2);
+    
+    printf("Resultado: %d + %d = %d\n", num1, num2, sum(num1, num2));
+
+}
+
+void clearBuffer(){
+    int c;
+    while((c = getchar()) != '\n' && c != EOF); 
+}
+
+int sum(int num1, int num2){
+    return num1 + num2;
+}
