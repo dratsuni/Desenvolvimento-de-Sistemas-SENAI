5cff23e6e9a43a0b7f9880a126ff6805eb1b9bb2
dratsuni <241861453+dratsuni@users.noreply.github.com>
2025-11-19 15:03:30 +0000

    algoritmo_calculadora.c adicionado

---
+++ simestre-01/logica-de-programacao/linguagem-C/exercicio-algoritimos-expressoes-aritimeticas/algoritmo_calculadora.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+
+
+typedef double (*PointerFunc) (double, double); 
+
+void cleanBuffer();
+double sum(double n1, double n2);
+double mult(double n1, double n2);
+double sub(double n1, double n2);
+double div(double n1, double n2);
+void calculate(double n1, double n2);
+
+int main(){
+    double n1, n2;
+
+    printf("Digite o valor do primeiro numero: ");
+    scanf("%lf", &n1);
+    cleanBuffer();
+    printf("Digite o valor do segundo numero: ");
+    scanf("%lf", &n2);
+    cleanBuffer();
+    calculate(n1, n2);
+    return 0;
+}
+
+void cleanBuffer(){
+    int c;
+    while((c = getchar()) != '\n' && c != EOF);
+}
+
+double sum(double n1, double n2){
+    return n1 + n2;
+}
+
+double mult(double n1, double n2){
+    return n1 * n2;
+}
+
+double sub(double n1, double n2){
+    return n1 - n2;
+}
+
+double div(double n1, double n2){
+    return n1 / n2;
+}
+void calculate(double n1, double n2){
+    PointerFunc func[4] = {sum, mult, sub, div};
+
+    char opArray[4] = {'+', '*', '-', '/'};
+    for (int i = 0; i < 4; i++){
+        printf("%.0lf  %c  %.0lf = %.2lf\n", n1, opArray[i], n2,func[i](n1, n2));
+    }
+}
