2a3793b364fc5b7a2b40bd786a36fc6b8a1b3c57
dratsuni <241861453+dratsuni@users.noreply.github.com>
2025-11-20 11:27:41 +0000

    update

--- simestre-01/logica-de-programacao/linguagem-C/exercicio-algoritimos-expressoes-aritimeticas/algoritmo_calculadora.c
+++ simestre-01/logica-de-programacao/linguagem-C/exercicio-algoritimos-expressoes-aritimeticas/algoritmo_calculadora.c
@@ -7,7 +7,7 @@ typedef struct {
     char operator;
 } Operation;
 
-void cleanBuffer();
+void clean_buffer();
 double sum(double n1, double n2);
 double mult(double n1, double n2);
 double sub(double n1, double n2);
@@ -15,20 +15,21 @@ double div(double n1, double n2);
 void calculate(double n1, double n2);
 
 int main(){
-    double n1, n2;
+    double n1 = 0;
+    double n2 = 0;
 
     printf("Digite o valor do primeiro numero: ");
     scanf("%lf", &n1);
-    cleanBuffer();
+    clean_buffer();
     printf("Digite o valor do segundo numero: ");
     scanf("%lf", &n2);
-    cleanBuffer();
+    clean_buffer();
     calculate(n1, n2);
     return 0;
 }
 
-void cleanBuffer(){
-    int c;
+void clean_buffer(){
+    int c = 0;
     while((c = getchar()) != '\n' && c != EOF);
 }
 
@@ -53,8 +54,10 @@ double div(double n1, double n2){
 }
 
 void calculate(double n1, double n2){
-    Operation operation[4] = {{sum, '+'}, {mult, '*'}, {sub, '-'}, {div, '/'} };
-    for (int i = 0; i < 4; i++){
+    Operation operation[] = {{sum, '+'}, {mult, '*'}, {sub, '-'}, {div, '/'} };
+    int operation_size = sizeof(operation) / sizeof(Operation);
+
+    for (int i = 0; i < operation_size; i++){
         printf("%.0lf %c %.0lf = %.2lf\n", n1, operation[i].operator, n2, operation[i].PointerFunc(n1, n2));
     }
 }
